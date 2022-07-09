be89ef1390232ee619d56b1ee6ec97431dd12dd2
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-07-10 01:35:45 +0200

    ref(authserv): Removed spring security

--- authserv/src/main/java/es/uma/config/SecurityConfig.java
+++ authserv/src/main/java/es/uma/config/SecurityConfig.java
@@ -1,16 +1,11 @@
 package es.uma.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class SecurityConfig {
 
-    @Bean
+/*    @Bean
     public SecurityFilterChain getFilterChain(HttpSecurity http) throws Exception {
         return http.authorizeRequests().and().build();
     }
@@ -18,5 +13,5 @@ public class SecurityConfig {
     @Bean
     public PasswordEncoder getPasswordEncoder() {
         return new BCryptPasswordEncoder(11);
-    }
+    }*/
 }
