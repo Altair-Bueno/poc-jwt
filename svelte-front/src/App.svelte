50f669b263a0936ca40caa7ca4d5d19721703a5c
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-07-12 18:42:42 +0200

    new(svelte-front): Created Svelte kit consumer application

---
+++ svelte-front/src/App.svelte
@@ -0,0 +1,99 @@
+<script lang="ts">
+    import {checkAuth, login, register, springPing} from "./lib/api.ts"
+
+    let credentials = {username: "", password: ""}
+    let session = undefined
+    let springPingContent = ''
+
+    // TODO: ideally, use some sort of library that manages authentication
+    // TODO: Store the JWT on localstorage/cookies
+
+    // Handlers
+    async function registerHandler() {
+        try {
+            await register(credentials)
+            displayBanner("Register successful")
+
+        } catch (exception) {
+            displayBanner("Register exception", exception)
+        }
+    }
+
+    async function loginHandler() {
+        try {
+            session = await login(credentials)
+            displayBanner("Login successful")
+
+        } catch (exception) {
+            displayBanner("Login exception", exception)
+        }
+    }
+
+    async function checkAuthHandler() {
+        try {
+            await checkAuth(session)
+            displayBanner("The user is authorised")
+        } catch (exception) {
+            displayBanner("The user is not authorised", exception)
+        }
+    }
+
+    async function springPingHandler() {
+        try {
+            const received = await springPing(session, springPingContent)
+            displayBanner(received)
+        } catch (exception) {
+            displayBanner("Ping error",exception)
+        }
+    }
+
+    // Utility functions
+
+    function displayBanner(message: string, exception?: Error) {
+        const kind = exception? 'Error' : 'Alert'
+        alert(`[${kind}]: ${message} \n${ exception ? exception : ""}`)
+    }
+</script>
+<h1>Welcome to the sample JWT microservice frontend</h1>
+<p>
+  The console contains more information about request/responses
+</p>
+
+<div>
+  <h2>Authserver</h2>
+  <p>Authserver is in charge of authentication. It also provides an endpoint to
+    check if the user is authorised or not</p>
+  <div>
+    <label>Username</label><input bind:value={credentials.username} type="text"><br>
+    <label>Password</label><input bind:value={credentials.password} type="text"><br>
+
+    <button on:click={registerHandler}>Register</button>
+    <button on:click={loginHandler}>Login</button>
+    <button on:click={checkAuthHandler}>
+      Check if the user is authorised
+    </button>
+  </div>
+</div>
+<br>
+
+<div>
+  <h2>Spring-ping</h2>
+  <p>Responds to an authorised user with the same content</p>
+  <div>
+    <input bind:value={springPingContent} type="text">
+    <button on:click={springPingHandler}>Send</button>
+  </div>
+</div>
+
+<br>
+<footer>
+  <h4>Debug information</h4>
+  <div>
+    username: {credentials?.username} <br>
+    password: {credentials?.password}
+  </div>
+  <div>
+    bearer token: {session?.access_token} <br>
+    refresh token: {session?.refresh_token}
+  </div>
+</footer>
---
+++ svelte-front/src/lib/api.ts
@@ -0,0 +1,118 @@
+const endpoints =  {
+    springPing: "http://localhost:9000",
+    authServ: "http://localhost:8000"
+}
+
+export type Credentials = {
+    username: string,
+    password: string
+}
+
+export interface JWTSession  {
+    access_token:string,
+    refresh_token:string,
+    token_type:string,
+    expires_in:string,
+}
+
+export async function checkAuth(session: JWTSession) {
+    const url = `${endpoints.authServ}/auth/check`
+    const request = {
+        method: "GET",
+        headers: {
+            "Authorization": `${session.token_type} ${session.access_token}`
+        }
+    }
+    const response = await fetch(url, request)
+
+    console.log([url, request, response])
+
+    if (response.ok) {
+        // nothing
+    } else {
+        throw Error("Unexpected response")
+    }
+}
+
+export async function register(credentials: Credentials){
+    const url = `${endpoints.authServ}/auth/register`
+    const request = {
+        body: JSON.stringify(credentials),
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }
+    const response = await fetch(url, request)
+
+    console.log([url, request, response])
+
+    if (response.ok) {
+        // nothing
+    } else {
+        throw Error("Unexpected response")
+    }
+}
+
+export async function refresh(session: JWTSession) {
+    const url = `${endpoints.authServ}/auth/refresh`
+    const request = {
+        body: JSON.stringify({"refresh_token": session.refresh_token}),
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }
+    const response = await fetch(url, request)
+    const json = await response.json()
+
+    console.log([url, request, response, json])
+
+    if (response.ok) {
+        return json as JWTSession
+    } else {
+        throw Error("Unexpected response")
+    }
+}
+
+export async function login(credentials:Credentials) {
+    const url = `${endpoints.authServ}/auth/login`
+    const request = {
+        body: JSON.stringify(credentials),
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    }
+    const response = await fetch(url, request)
+    const json = await response.json()
+
+    console.log([url, request, response, json])
+
+    if (response.ok) {
+        return json as JWTSession
+    } else {
+        throw Error("Unexpected response")
+    }
+}
+
+export async function springPing(session: JWTSession, content: string) {
+    const url = `${endpoints.springPing}/`
+    const request = {
+        method: "POST",
+        body: content,
+        headers: {
+            "Content-Type": "text/plain",
+            "Authorization": `${session.token_type} ${session.access_token}`
+        }
+    }
+    const response = await fetch(url, request)
+
+    console.log([url, request, response])
+
+    if (response.ok) {
+        return await response.text()
+    } else {
+        throw Error("Unexpected response")
+    }
+}
