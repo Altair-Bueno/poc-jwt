e64d1f83905bd0f5e5f9b2ffb9bf0c373a2b3d6c
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-07-14 15:24:36 +0200

    ref(svelte-front): Prettier now formats Svelte components

--- svelte-front/src/App.svelte
+++ svelte-front/src/App.svelte
@@ -1,36 +1,34 @@
 <script lang="ts">
-    import {credentialStore} from "./lib/credentials";
-    import SpringPing from "./lib/components/SpringPing.svelte";
-    import AuthServ from "./lib/components/AuthServ.svelte";
-    import {displayBanner} from "./lib/util";
+  import { credentialStore } from "./lib/credentials";
+  import SpringPing from "./lib/components/SpringPing.svelte";
+  import AuthServ from "./lib/components/AuthServ.svelte";
+  import { displayBanner } from "./lib/util";
 
-    function accessTokenToClipboard() {
-        const accessToken = $credentialStore?.accessToken.token
-        if (accessToken) {
-          navigator.clipboard.writeText(accessToken)
-        } else {
-            displayBanner("The clipboard is empty", new Error())
-        }
+  function accessTokenToClipboard() {
+    const accessToken = $credentialStore?.accessToken.token;
+    if (accessToken) {
+      navigator.clipboard.writeText(accessToken);
+    } else {
+      displayBanner("The clipboard is empty", new Error());
     }
+  }
 </script>
+
 <div class="container">
   <div>
     <h1>Welcome to the sample JWT microservice frontend</h1>
-    <p>
-      The console contains more information about request/responses
-    </p>
+    <p>The console contains more information about request/responses</p>
   </div>
-  <AuthServ/>
-  <SpringPing/>
+  <AuthServ />
+  <SpringPing />
   <footer class="bg-cornflowerblue">
     <h4>Stores content</h4>
     <div>
-      access token: {$credentialStore?.accessToken.token} <br>
+      access token: {$credentialStore?.accessToken.token} <br />
       refresh token: {$credentialStore?.refreshToken}
     </div>
-    <button on:click={accessTokenToClipboard}
-      class="btn btn-primary"
-    >Copy access token to clipboard
+    <button on:click={accessTokenToClipboard} class="btn btn-primary"
+      >Copy access token to clipboard
     </button>
   </footer>
 </div>
--- svelte-front/src/lib/components/SpringPing.svelte
+++ svelte-front/src/lib/components/SpringPing.svelte
@@ -1,25 +1,26 @@
 <script lang="ts">
-  import {springPingClient} from "../config";
-  import {displayBanner} from "../util";
+  import { springPingClient } from "../config";
+  import { displayBanner } from "../util";
 
-  let springPingContent = ''
+  let springPingContent = "";
 
   async function springPingHandler() {
-      try {
-          const response = await springPingClient.rootPost(springPingContent)
-          const data = response.data
-          console.log(data)
-          displayBanner(data)
-      } catch (exception) {
-          displayBanner("Ping error", exception)
-      }
+    try {
+      const response = await springPingClient.rootPost(springPingContent);
+      const data = response.data;
+      console.log(data);
+      displayBanner(data);
+    } catch (exception) {
+      displayBanner("Ping error", exception);
+    }
   }
 </script>
+
 <div>
   <h2>Spring-ping</h2>
   <p>Responds to an authorised user with the same content</p>
   <div>
-    <textarea bind:value={springPingContent} class="form-control" type="text"></textarea>
+    <textarea bind:value={springPingContent} class="form-control" type="text" />
     <button class="btn btn-primary" on:click={springPingHandler}>Send</button>
   </div>
 </div>
