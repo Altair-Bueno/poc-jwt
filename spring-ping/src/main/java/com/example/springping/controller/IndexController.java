71e87d1051c371657df2f1c3e81ea4ac21f4356a
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-07-14 14:28:09 +0200

    fix(spring-ping): Replaced text/plain with application/json

--- spring-ping/src/main/java/com/example/springping/controller/IndexController.java
+++ spring-ping/src/main/java/com/example/springping/controller/IndexController.java
@@ -6,11 +6,11 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @CrossOrigin
 public class IndexController {
-    @GetMapping("/")
+    @GetMapping(value = "/",consumes = "application/json", produces = "application/json")
     public String index() {
         return "Hello from Spring ping!";
     }
-    @PostMapping("/")
+    @PostMapping(value = "/",consumes = "application/json", produces = "application/json")
     public String index(@RequestBody String content) {
         return content;
     }
