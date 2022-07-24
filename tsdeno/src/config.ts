396078c80534abc7643800597dbcf6bd948a2b4b
Altair-Bueno <67512202+Altair-Bueno@users.noreply.github.com>
2022-07-24 20:50:22 +0200

    new(tsdeno): config file can be moved

--- tsdeno/src/config.ts
+++ tsdeno/src/config.ts
@@ -1,36 +1,39 @@
 import { parse } from "flags/mod.ts";
 import { AlgorithmInput } from "djwt/algorithm.ts";
-import { info } from "log/mod.ts"
+import { info, warning } from "log/mod.ts";
 
-const CONFIG_FILENAME = 'oak.json'
+const CONFIG_FILENAME_ENV = "OAK_CONFIG_FILE"
+const CONFIG_FILENAME = Deno.env.get(CONFIG_FILENAME_ENV) || "oak.json";
 
 export interface Config {
   port: number;
   publicKey: string;
   algorithm: AlgorithmInput;
+  hostname: string
 }
 
 const defaultConfig = {
   port: 9200,
   algorithm: "RS256",
+  hostname: "0.0.0.0"
 };
 
 const args = parse(Deno.args);
 
 let file = {};
 try {
+  info(`Loading config file: ${CONFIG_FILENAME}. Note: set the location of the config file using \`${CONFIG_FILENAME_ENV}\``)
   const content = await Deno.readTextFile(CONFIG_FILENAME);
-  file = JSON.parse(content)
+  file = JSON.parse(content);
 } catch {
-  info(`No configuration file provided. Expected ${CONFIG_FILENAME}`);
+  warning(`No configuration file provided. Expected ${CONFIG_FILENAME}`);
 }
 
-const config = { 
-  ...defaultConfig, 
+const config = {
+  ...defaultConfig,
   ...file,
-  ...args 
+  ...args,
 } as unknown as Config;
-
 
 info("Configuration loaded succesfully");
 info(config);
